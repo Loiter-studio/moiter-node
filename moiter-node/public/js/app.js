fcfb23a1444e2a488e3a852d19b821e4c5dca1eb
rathinho <rathinho17@gmail.com>
2013-03-27 10:21:46 +0800

    Delete all

--- moiter-node/public/js/app.js
+++
@@ -1,9 +0,0 @@
-/* Global View */
-
-var app = app || {};
-var socket = io.connect("http://localhost:3001");
-
-// kick things off by creating the `App`
-
-new app.MainView();
-new app.LeftSidebarView();
--- public/js/collections/companies.js
+++
@@ -1,8 +0,0 @@
-/* User Collection */
-
-var app = app || {};
-
-app.CompanyList = Backbone.Collection.extend({
-	model: app.Company,
-	localStorage: new Backbone.LocalStorage("companyList");
-});
